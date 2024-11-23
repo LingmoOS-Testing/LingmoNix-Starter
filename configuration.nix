892fb8e55595982409c2a42cc8aa205134acbd74
Lycaon Constantine Cayde <114994536+arkimium@users.noreply.github.com>
2024-11-24 01:13:55 +0800

    Update configuration.nix -> Give up hardware configuration.

--- configuration.nix
+++ configuration.nix
@@ -5,11 +5,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   # boot.loader.grub.efiSupport = true;
