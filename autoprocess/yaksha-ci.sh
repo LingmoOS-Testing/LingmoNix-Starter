dc41fbd2148ea223afeb9a1c9bbc64355825ec2c
Lycaon Constantine Cayde <114994536+arkimium@users.noreply.github.com>
2025-01-29 14:26:14 +0800

    Update yaksha-ci.sh

--- autoprocess/yaksha-ci.sh
+++ autoprocess/yaksha-ci.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# 打印黄色的提示信息
+
 echo -e "\033[1;33m==> Fully Upgrading System...\033[0m"
+./yaksha-update.sh
 
-# 调用第一个脚本来检查是否需要更新
-./update-check.sh
-
-# 检查第一个脚本是否成功执行
 if [[ $? -ne 0 ]]; then
-  echo "Update check failed or update aborted. Exiting upgrade process."
+  echo "Update check failed or update aborted. Maybe the system is latest or the network problems? Exiting upgrade process."
   exit 1
 fi
 
 # 将源码复制到 /etc/nixos 目录
-sudo cp -r ~/yaksha/* /etc/nixos/
+sudo cp -r ~/LingmoNix-Starter/* /etc/nixos/
 
 # 执行 nixos-rebuild switch 来应用更改
 sudo nixos-rebuild switch
