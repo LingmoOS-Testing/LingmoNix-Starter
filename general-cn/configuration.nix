ec58940827b6e7f2f92c1ca18989adc7ed813337
Lycaon Constantine Cayde <114994536+arkimium@users.noreply.github.com>
2025-02-04 22:17:37 +0800

    更新 configuration.nix

--- general-cn/configuration.nix
+++ general-cn/configuration.nix
@@ -100,7 +100,18 @@
     jetbrains-mono
     texlivePackages.opensans
     inter-nerdfont
-    nerdfonts     # Deprecated since NixOS 25.05
+    # nerdfonts     # Deprecated since NixOS 25.05
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.zed-mono
+    nerd-fonts.symbols-only
+    nerd-fonts.terminess-ttf
+    nerd-fonts.ubuntu
+    nerd-fonts.ubuntu-mono
+    nerd-fonts.ubuntu-sans
+    nerd-fonts.victor-mono
+    nerd-fonts.noto
+    nerd-fonts.lilex
+    nerd-fonts.liberation
     roboto
     roboto-mono
   ];
